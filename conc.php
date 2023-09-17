daf9fe321acb8bbef232e380bc26ab69111a12d2
mhmtali1 <134264863+mhmtali1@users.noreply.github.com>
2023-09-17 23:42:50 +0300

    php proje

---
+++ conc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+    <?php 
+    $uname = $_POST["name"];
+    $nname = $_POST["nick"];
+
+    echo $uname . "" . $uname;
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+    <form action="conc.php" method="post">
+        uname : <input type="text" name="name"><br>
+        nname : <input type="text" name="nick">
+        <input type="submit" value="send">
+    </form>
+</body>
+</html>
